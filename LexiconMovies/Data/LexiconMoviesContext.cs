32c301eb110745b30968b2d4c349f72af64138a2
Adrian Lozano <adrian@xenotype.com>
2019-01-17 17:17:18 +0100

    Add project files.

---
+++ LexiconMovies/Data/LexiconMoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LexiconMovies.Models
+{
+    public class LexiconMoviesContext : DbContext
+    {
+        public LexiconMoviesContext (DbContextOptions<LexiconMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
